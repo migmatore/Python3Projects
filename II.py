3b8448a362631467d8966a4e063a3e29afb07cd1
Vova <kazakvova201@gmail.com>
2018-02-05 22:02:33 +0300

    Update II.py and lib/lib1.py and lib/classes/Base.py

--- II.py
+++ II.py
@@ -1,10 +1,11 @@
 from lib.lib1 import STRS
 from lib.classes.Base import Base
 
+
 class II(Base):
     def __init__(self, msg):
         self.msg = msg
-    
+
         if str(self.msg) is None:
             print("!!!")
 
@@ -12,30 +13,51 @@ class II(Base):
             self.speak(self.msg)
 
     def speak(self, msg):
-        if self.msg == STRS[1] or self.msg == STRS[1.1]:
+        if self.msg == STRS[1] or self.msg == STRS[1.2]:
             print(self._Speak({
                 1: "Привет"
             }))
 
         elif self.msg == STRS[2] or self.msg == STRS[2.2]:
-            print(self.plus())
-        
-    def _Speak(self, txt):
-        super()._Speak(txt)
+            self.plus()
+
+        elif self.msg == STRS[3] or self.msg == STRS[3.2]:
+            self.minus()
+
+        elif self.msg == STRS[4] or self.msg == STRS[4.2]:
+            self.umn()
+
+        elif self.msg == STRS[5] or self.msg == STRS[5.2]:
+            self.delen()
+
+        elif self.msg == STRS[6] or self.msg == STRS[6.2]:
+            self.step()
+
+        else:
+            print("Введите что-нобудь!")
+
+    @staticmethod
+    def _Speak(txt):
+        return str(txt[1])
 
     def plus(self):
-        try:
-            num1 = int(input("> "))
-            num2 = int(input("> "))
+        super().plus()
 
-            return(num1 + num2)
+    def minus(self):
+        super().minus()
 
-        except:
-            return "Введите число!"
+    def umn(self):
+        super().umn()
+
+    def delen(self):
+        super().delen()
+
+    def step(self):
+        super().step()
 
 
 if __name__ == "__main__":
     while True:
         msg = str(input("> "))
-    
+
         ii = II(msg)
--- lib/classes/Base.py
+++ lib/classes/Base.py
@@ -1,3 +1,54 @@
 class Base:
-    def _Speak(self, txt):
-        return str(txt[1])
+    def plus(self):
+        try:
+            num1 = int(input("> "))
+            num2 = int(input("> "))
+
+            print(num1 + num2)
+
+        except:
+            print("Введите число!")
+
+    def minus(self):
+        try:
+            num1 = int(input("> "))
+            num2 = int(input("> "))
+
+            print(num1 - num2)
+
+        except:
+            print("Введите число!")
+
+    def umn(self):
+        try:
+            num1 = int(input("> "))
+            num2 = int(input("> "))
+
+            print(num1 * num2)
+
+        except:
+            print("Введите число!")
+
+    def delen(self):
+        try:
+            num1 = int(input("> "))
+            num2 = int(input("> "))
+
+            if num2 != 0:
+                print(num1 / num2)
+
+            else:
+                print("Нельзя делить на 0!")
+
+        except:
+            print("Введите число!")
+
+    def step(self):
+        try:
+            num1 = int(input("> "))
+            num2 = int(input("> "))
+
+            print(num1 ** num2)
+
+        except:
+            print("Введите число!")
